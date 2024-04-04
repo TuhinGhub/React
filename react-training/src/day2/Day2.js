f0dd0c7fcfcaf7224bfcb866119c4a5ca1ab8542
TuhinGhub <tuhin.paul@successive.tech>
2024-04-04 20:24:10 +0530

    edited

---
+++ react-training/src/day2/Day2.js
@@ -0,0 +1,48 @@
+import Counter2 from "./Counter2"
+import CartContext from './CartContext'
+import Clock from './Clock'
+import CountdownTimer from './CountdownTimer'
+import CounterWithStep from './CounterWithStep'
+import EmployeeSalary from './EmployeeSalary'
+import LanguageContext from './LanguageContext'
+import Notification from './Notification'
+import Parent from './Parent'
+import PersonForm from './PersonForm'
+import Preferences from './Preferences'
+import RandomNumberGenerator from './RandomNumberGenerator'
+import Slideshow from './Slideshow'
+import StudentList from  './StudentList'
+import TaskList2 from './TaskList2'
+import ThemeContext from  './ThemeContext'
+import ToDoList from './ToDoList'
+import ClipboardCopyButton from './ClipboardCopyButton'
+import VotingApp from './VotingApp'
+const Day2 = () => {
+  return (
+    <div>
+        <Counter2 />
+        <CartContext/>
+        <Clock/>
+        <CountdownTimer/>
+        <CounterWithStep/>
+        <EmployeeSalary/>
+        <LanguageContext/>
+        <Notification/>
+        <Parent/>
+        <PersonForm/>
+        <Preferences/>
+        <RandomNumberGenerator/>
+        <Slideshow/>
+        <StudentList/>
+        <TaskList2/>
+        <ThemeContext/>
+        <ToDoList/>
+        <ClipboardCopyButton/>
+        <VotingApp/>
+
+
+    </div>
+  )
+}
+
+export default Day2
